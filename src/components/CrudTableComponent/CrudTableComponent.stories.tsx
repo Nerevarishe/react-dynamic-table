1d0f6041e723b011fd1c6727c97f8dec3c8cd616
Ilia Smorodin <smorodin.it@gmail.com>
2021-10-22 16:27:05 +0300

    init commit

---
+++ src/components/CrudTableComponent/CrudTableComponent.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { CrudTableComponent } from './CrudTableComponent';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Tables/CRUD Table Component',
+  component: CrudTableComponent,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof CrudTableComponent>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof CrudTableComponent> = (args) => (
+  <CrudTableComponent {...args} />
+);
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {};
---
+++ src/components/CrudTableComponent/CrudTableComponent.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+export const CrudTableComponent: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
