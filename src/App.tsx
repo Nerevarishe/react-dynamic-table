27c23a414dfbc7c3b1caeab3fa5a87925ce1e9b6
Ilia Smorodin <smorodin.it@gmail.com>
2021-10-27 08:57:28 +0300

    rendering table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  CrudTableComponent,
+  CrudTableComponentSettings,
+} from './components/CrudTableComponent/CrudTableComponent';
+import testData from './components/CrudTableComponent/testData.json';
+
+interface PropertyModel {
+  readonly code: string;
+  readonly dataType: string;
+  readonly editable: boolean;
+  readonly frontendInput: string;
+  readonly id: string;
+  readonly label: string;
+  readonly multiple: boolean;
+  readonly required: boolean;
+}
+
+const settings: CrudTableComponentSettings<PropertyModel> = {
+  actions: [
+    {
+      type: 'add',
+      handler: () => console.log('ADD_ACTION'),
+    },
+  ],
+  fields: [
+    {
+      header: 'Тип инфомрации',
+      name: 'dataType',
+      render: (object) => <>{object.dataType}</>,
+    },
+    {
+      header: 'Тип компонента',
+      name: 'frontendInput',
+      render: (object) => <>{object.frontendInput}</>,
+    },
+    {
+      header: 'Код',
+      name: 'code',
+      render: (object) => <>{object.code}</>,
+    },
+    {
+      header: 'Наименование',
+      name: 'label',
+      render: (object) => <>{object.label}</>,
+    },
+    {
+      header: 'Множественный выбор',
+      name: 'multiple',
+      render: (object) => <>{`${object.multiple}`}</>,
+    },
+    {
+      header: 'Обязательный',
+      name: 'required',
+      render: (object) => <>{`${object.required}`}</>,
+    },
+    {
+      header: 'Редактируемый',
+      name: 'editable',
+      render: (object) => <>{`${object.editable}`}</>,
+    },
+  ],
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CrudTableComponent data={testData} settings={settings} />
     </div>
   );
 }
--- src/components/CrudTableComponent/CrudTableComponent.tsx
+++ src/components/CrudTableComponent/CrudTableComponent.tsx
@@ -1,10 +1,92 @@
-import React, {FC} from 'react';
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
-export const CrudTableComponent: FC = () => {
-    return (
-        <div>
+// enum ActionTypes {
+//   ADD = 'add',
+//   EDIT = 'edit',
+//   DELETE = 'delete',
+//   DETAILS = 'details',
+// }
 
-        </div>
-    );
-};
+interface MinimalDataModel {
+  id: string;
+  [key: string]: any;
+}
 
+export interface Action {
+  // type: ActionTypes;
+  type: 'add' | 'edit' | 'delete' | 'details';
+  handler: (id?: string) => void;
+}
+
+export interface Field<DataModel> {
+  header: string;
+  name: string;
+  render: (object: DataModel) => JSX.Element;
+}
+
+export interface CrudTableComponentSettings<DataModel> {
+  actions: Action[];
+  fields: Field<DataModel>[];
+}
+
+export interface CrudTableComponentProps<DataModel> {
+  data: DataModel[];
+  settings: CrudTableComponentSettings<DataModel>;
+  store?: any;
+}
+
+export function CrudTableComponent<DataModel extends MinimalDataModel>(
+  props: CrudTableComponentProps<DataModel>
+): JSX.Element {
+  // const getDataObjectFields = (removeId: boolean = true): string[] => {
+  //   return removeId
+  //     ? Object.keys(props.data[0]).filter((fieldName) => fieldName !== 'id')
+  //     : Object.keys(props.data[0]);
+  // };
+
+  const getDataObjectFields = (removeId: boolean = true): string[] => {
+    return removeId
+      ? Object.keys(props.data[0]).filter((fieldName) => fieldName !== 'id')
+      : Object.keys(props.data[0]);
+  };
+
+  const getHeader = (): JSX.Element[] => {
+    return props.settings.fields.map((field) => (
+      <TableCell key={field.header}>{field.header}</TableCell>
+    ));
+  };
+
+  const getRowsData = (): JSX.Element[] => {
+    const fields = getDataObjectFields();
+    console.log(fields);
+
+    return props.data.map((row, idx) => (
+      <TableRow key={idx}>
+        {fields.map((field) => (
+          <>
+            <TableCell key={field}>{props.data[idx][field]}</TableCell>
+          </>
+        ))}
+      </TableRow>
+    ));
+  };
+
+  return (
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>{getHeader()}</TableHead>
+          <TableBody>{getRowsData()}</TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
